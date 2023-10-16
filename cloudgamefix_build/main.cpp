22fec8031bec5659bba70c5aa8aa2154db394549
eGlhb2p1emk <atzxqr8w96i@outlook.com>
2023-10-16 05:50:29 +0000

    删除文件 cloudgamefix_build

--- cloudgamefix_build/main.cpp
+++
@@ -1,9 +0,0 @@
-#include"cloudgamefix.hpp"
-
-int main()
-{
-	std::string text = cloudgameZero::Foundation::Tool::function::getDataFromUrl("https://api.xygeng.cn/one");
-	std::cout << text << "\n";
-	system("pause");
-	return 0;
-}
